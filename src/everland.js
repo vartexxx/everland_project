6d9f8658d806d78a74f0a26e35f73d46d0308c56
vartexxx <vartexxx29@yandex.ru>
2022-08-16 17:10:34 +0500

    feat: добавлен адаптив, кроме JS

---
+++ src/everland.js
@@ -0,0 +1,4 @@
+const buttonNext = document.querySelector('.everland__button_right');
+const buttonPrevious = document.querySelector('.everland__button_left');
+const slideNumber = document.querySelector('.everland__slider-number')
+const slides = document.getElementsByClassName('everland');
