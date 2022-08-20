35f417e8d7c3b681df01dbc3ebb84fdcdf9889f4
Kostya Buzunov <buzunov.constantine@yandex.ru>
2022-08-20 19:13:39 +0300

    feat: сделать выбор радиокнопок через js

---
+++ src/donation.js
@@ -0,0 +1,22 @@
+/* Получить доступ к dom элементам секции donations*/
+const donation250 = document.querySelector("#donation_250");
+const donation500 = document.querySelector("#donation_500");
+const donation750 = document.querySelector("#donation_750");
+const donation1000 = document.querySelector("#donation_1000");
+const donationOther = document.querySelector("#donation_other");
+
+/* Получить доступ к dom элементам секции support */
+const support250 = document.querySelector("#support_250");
+const support500 = document.querySelector("#support_500");
+const support750 = document.querySelector("#support_750");
+const support1000 = document.querySelector("#support_1000");
+const supportOther = document.querySelector("#support_other");
+
+donation250.addEventListener("click", checkStatus);
+
+function checkStatus(evt) {
+  if ((evt.target.checked = true)) {
+    console.log(evt.target);
+    support250.setAttribute("checked", true);
+  }
+}
