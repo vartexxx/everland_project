426883c94cdf221601b37f9ae5f1efab46174424
Maxim Chalikov <aracctur@yandex.ru>
2022-08-24 15:40:32 +0300

    fix: поправил отступы и разметку кода в файлах, согласно 7.3

--- src/header.js
+++ src/header.js
@@ -2,19 +2,6 @@ const imageMenu = document.querySelector('.menu__image');
 const containerBigMenu = document.querySelector('.menu__big-container');
 const menuOpenIcon = document.querySelector('.menu__open-icon');
 
-
-
-menuOpenIcon.addEventListener('click', () => {
-  imageMenu.classList.toggle('menu__image_opened');
-  if (containerBigMenu.style.visibility === 'visible') {
-    containerBigMenu.style.visibility = 'hidden';
-    containerBigMenu.style.opacity = '0';
-  } else {
-    containerBigMenu.style.visibility = 'visible'
-    containerBigMenu.style.opacity = '1';
-  }
-});
-
 const buttonMenuOProjecte = document.querySelector('.menu__bigtitle-mobile_o-projecte');
 const buttonMenuLyudyam = document.querySelector('.menu__bigtitle-mobile_lyudyam');
 const buttonMenuUslugi = document.querySelector('.menu__bigtitle-mobile_uslugi');
@@ -26,6 +13,17 @@ const listMenuUslugi = document.querySelectorAll('.menu__big-link_uslugi');
 const statusImageButtonOProjecte = document.querySelector('.menu__button-status-mobile_o-projecte');
 const statusImageButtonLyudyam = document.querySelector('.menu__button-status-mobile_lyudyam');
 const statusImageButtonUslugi = document.querySelector('.menu__button-status-mobile_uslugi');
+
+menuOpenIcon.addEventListener('click', () => {
+  imageMenu.classList.toggle('menu__image_opened');
+  if (containerBigMenu.style.visibility === 'visible') {
+    containerBigMenu.style.visibility = 'hidden';
+    containerBigMenu.style.opacity = '0';
+  } else {
+    containerBigMenu.style.visibility = 'visible'
+    containerBigMenu.style.opacity = '1';
+  }
+});
 
 buttonMenuOProjecte.addEventListener('click', () => {
   listMenuOProjecte.forEach((el) => {
@@ -40,6 +38,7 @@ buttonMenuLyudyam.addEventListener('click', () => {
   });
   statusImageButtonLyudyam.classList.toggle('menu__button-status-mobile_opened')
 });
+
 buttonMenuUslugi.addEventListener('click', () => {
   listMenuUslugi.forEach((el) => {
     el.classList.toggle('menu__big-link_opened')
